a0c6600914677ebda9060c2f191f135ebcaa6e3e
303-10\megait <asdf@gmail.com>
2022-03-15 17:13:56 +0900

    [정혜원] - 최초 파일 추가

---
+++ src/test/java/com/ffaikrw/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffaikrw.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
